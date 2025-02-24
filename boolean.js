241fc16b0067fc2a674cf658a825c77c152a5db5
bill-208 <162450903+bill-208@users.noreply.github.com>
2025-02-24 08:49:09 -0300

    Add files via upload

---
+++ boolean.js
@@ -0,0 +1,18 @@
+// Operdores de comparação
+// Operadores de igualdade (compara apenas o valor)
+
+console.log("1" == 1)
+console.log(true == 1)
+console.log(false == 0)
+console.log(false == "")
+
+// Operador de igualdade estrita
+// (compara valor e tipo)
+console.log("1" === 1)
+console.log(true === 1)
+console.log(false === 0)
+
+// Verificar tipo da variável
+console.log(typeof("1")) // -> texto (string)
+console.log(typeof(1)) // -> Nunber
+console.log(typeof(true)) // -> boolean
---
+++ calculadora.js
@@ -0,0 +1,12 @@
+let valor1 = 2   
+let valor2 = 4
+
+let soma = valor1 + valor2
+let sub = valor1 - valor2
+let multi = valor1 * valor2
+let div = valor1 / valor2
+
+console.log("Soma = " + soma)
+console.log("Sub = " + sub)
+console.log("Multiplição = " + multi)
+console.log("Divisão = " + div)
