da502faa166a82b3fccfb9bc68eb00de73eb0261
bill-208 <162450903+bill-208@users.noreply.github.com>
2025-03-10 08:22:32 -0300

    Add files via upload

---
+++ exercicio.js
@@ -0,0 +1,16 @@
+let peso = 65
+
+let qtd_agua = peso * 35 / 1000
+
+console.log("Quantidade de agua = " + qtd_agua)
+
+const Av1=10.0
+const Av2=7.0
+const Av3=4.0
+const media=(Av1+Av2+Av3) /3
+if(media>=6.0){
+    console.log("Aprovado com ");
+}else{
+    console.log("Reprovado");
+}
+console.log("Media de " + media)
